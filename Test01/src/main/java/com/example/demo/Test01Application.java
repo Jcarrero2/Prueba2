306e3e6041ae16e02c4128b90b496cc9fa7451b9
JCARRERO <JCARRERO@192.168.1.135>
2020-09-27 12:01:47 +0200

    creada rama 1

--- Test01/src/main/java/com/example/demo/Test01Application.java
+++ Test01/src/main/java/com/example/demo/Test01Application.java
@@ -9,6 +9,7 @@ public class Test01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Test01Application.class, args);
 		System.out.println("P1");
+		System.out.println("creada rama1");
 	}
 
 }
